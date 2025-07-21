68260bca79db9ac5d1e2d798e3e3794dea653bfe
rudit05 <166696260+rudit05@users.noreply.github.com>
2025-07-22 01:51:02 +0530

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -16,12 +16,10 @@ module tb_TrainTrafficControl;
     .grant(grant)
   );
 
-  // Clock: 10ns period
   initial clk = 0;
   always #5 clk = ~clk;
 
   initial begin
-    // Initialize
     reset = 1;
     train_request = 4'b0000;
     train_done = 0;
@@ -29,71 +27,63 @@ module tb_TrainTrafficControl;
     #20;
     reset = 0;
 
-    // ---------- Test IDLE ----------
-    train_request = 4'b0000;  // no requests
+    //Test IDLE
+    train_request = 4'b0000;  
     train_done = 0;
     #30;
-    // Should stay in IDLE, grant = 000
 
-    // ---------- Test GRANT_T1 ----------
-    train_request = 4'b0001;  // T1 request
+    //Test GRANT_T1
+    train_request = 4'b0001;  
     train_done = 0;
     #10;
-    // Should grant T1: grant = 001
     #30;
-    train_done = 1;           // indicate T1 crossing done
+    train_done = 1;        
     #10;
     train_done = 0;
     #20;
-    // After done, no request -> IDLE
 
-    // ---------- Test GRANT_T2 ----------
-    train_request = 4'b0010;  // T2 request
+    //Test GRANT_T2
+    train_request = 4'b0010;  
     train_done = 0;
     #10;
-    // Should grant T2: grant = 010
     #30;
-    train_done = 1;           // T2 done
+    train_done = 1;          
     #10;
     train_done = 0;
     #20;
-    train_request = 4'b0000;  // no requests
+    train_request = 4'b0000;  
     #20;
 
-    // ---------- Test GRANT_T3 ----------
-    train_request = 4'b0100;  // T3 request
+    //Test GRANT_T3
+    train_request = 4'b0100;  
     train_done = 0;
     #10;
-    // Should grant T3: grant = 011
     #30;
-    train_done = 1;           // T3 done
+    train_done = 1;     
     #10;
     train_done = 0;
     #20;
     train_request = 4'b0000;
     #20;
 
-    // ---------- Test GRANT_T4 ----------
-    train_request = 4'b1000;  // T4 request
+    //Test GRANT_T4
+    train_request = 4'b1000;  
     train_done = 0;
     #10;
-    // Should grant T4: grant = 100
     #30;
-    train_done = 1;           // T4 done
+    train_done = 1;          
     #10;
     train_done = 0;
     #20;
     train_request = 4'b0000;
     #20;
 
-    // ---------- Test Timeout from GRANT_T1 ----------
-    train_request = 4'b0001;  // T1 request
+    //Test Timeout from GRANT_T1 
+    train_request = 4'b0001;  
     train_done = 0;
-    // Wait longer than TIMEOUT cycles (TIMEOUT=5, clock=10ns, so 50ns + margin)
+    
     #70;
-    // Should timeout to IDLE automatically
-
-    // Finish simulation
+    
     $finish;
   end
 
