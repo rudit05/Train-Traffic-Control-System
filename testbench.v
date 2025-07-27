802c824c5310232201ea275530d9798839e36fdd
rudit05 <166696260+rudit05@users.noreply.github.com>
2025-07-28 00:59:41 +0530

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -1,95 +1,64 @@
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 
-module tb_TrainTrafficControl;
+module TrainTrafficControl_tb;
 
-  reg clk;
-  reg reset;
-  reg [3:0] train_request;
-  reg train_done;
-  wire [2:0] grant;
 
-  TrainTrafficControl dut (
-    .clk(clk),
-    .reset(reset),
-    .train_request(train_request),
-    .train_done(train_done),
-    .grant(grant)
-  );
+    reg clk;
+    reg reset;
+    reg [3:0] train_request;
+    reg train_done;
 
-  initial clk = 0;
-  always #5 clk = ~clk;
+    wire [2:0] grant;
 
-  initial begin
-    reset = 1;
-    train_request = 4'b0000;
-    train_done = 0;
+    TrainTrafficControl uut (
+        .clk(clk),
+        .reset(reset),
+        .train_request(train_request),
+        .train_done(train_done),
+        .grant(grant)
+    );
 
-    #20;
-    reset = 0;
+    always #5 clk = ~clk;
 
-    //Test IDLE
-    train_request = 4'b0000;  
-    train_done = 0;
-    #30;
+    initial begin
+        clk = 0;
+        reset = 1;
+        train_request = 4'b0000;
+        train_done = 0;
 
-    //Test GRANT_T1
-    train_request = 4'b0001;  
-    train_done = 0;
-    #10;
-    #30;
-    train_done = 1;        
-    #10;
-    train_done = 0;
-    #20;
+        $dumpfile("TrainTrafficControl_tb.vcd");
+        $dumpvars(0, TrainTrafficControl_tb);
+        
+        #10 reset = 0;
 
-    //Test GRANT_T2
-    train_request = 4'b0010;  
-    train_done = 0;
-    #10;
-    #30;
-    train_done = 1;          
-    #10;
-    train_done = 0;
-    #20;
-    train_request = 4'b0000;  
-    #20;
+// Test 1: Request by Train 1
+        #10 train_request = 4'b0001;
+        #50 train_done = 1;  
+        #10 train_done = 0;
 
-    //Test GRANT_T3
-    train_request = 4'b0100;  
-    train_done = 0;
-    #10;
-    #30;
-    train_done = 1;     
-    #10;
-    train_done = 0;
-    #20;
-    train_request = 4'b0000;
-    #20;
+// Test 2: Simultaneous request from Train 1 and 4
+        #10 train_request = 4'b1001; 
+        #50 train_done = 1;
+        #10 train_done = 0;
 
-    //Test GRANT_T4
-    train_request = 4'b1000;  
-    train_done = 0;
-    #10;
-    #30;
-    train_done = 1;          
-    #10;
-    train_done = 0;
-    #20;
-    train_request = 4'b0000;
-    #20;
+// Test 3: Request by Train 3
+        #10 train_request = 4'b0100;
+        #50 train_done = 1;
+        #10 train_done = 0;
 
-    //Test Timeout from GRANT_T1 
-    train_request = 4'b0001;  
-    train_done = 0;
-    
-    #70;
-    
-    $finish;
-  end
+// Test 4: done signal not sent
+        #10 train_request = 4'b0010;
+        #60; 
 
-  initial begin
-    $display("Time\treset\ttrain_req\ttrain_done\tgrant");
-    $monitor("%0t\t%b\t%04b\t\t%b\t\t%03b", $time, reset, train_request, train_done, grant);
-  end
+// Test 5: Multiple Quick Swiching
+        train_done = 1;
+        train_request = 4'b1100;
+        #10 train_done = 0;
+        #20 train_done = 1;
+        train_request = 4'b0000;
+        #10 train_done = 0;
+
+        #50 $finish;
+    end
 
 endmodule
