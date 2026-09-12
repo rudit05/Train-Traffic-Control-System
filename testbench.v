63d188f354af8795bc53c1eab05cb5173b080a69
rudit05 <166696260+rudit05@users.noreply.github.com>
2026-09-13 00:59:07 +0530

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -1,64 +1,24 @@
-`timescale 1ns / 1ps
-
-module TrainTrafficControl_tb;
-
-
-    reg clk;
-    reg reset;
-    reg [3:0] train_request;
-    reg train_done;
-
-    wire [2:0] grant;
-
-    TrainTrafficControl uut (
-        .clk(clk),
-        .reset(reset),
-        .train_request(train_request),
-        .train_done(train_done),
-        .grant(grant)
-    );
-
-    always #5 clk = ~clk;
-
-    initial begin
-        clk = 0;
-        reset = 1;
-        train_request = 4'b0000;
-        train_done = 0;
-
-        $dumpfile("TrainTrafficControl_tb.vcd");
-        $dumpvars(0, TrainTrafficControl_tb);
-        
-        #10 reset = 0;
-
-// Test 1: Request by Train 1
-        #10 train_request = 4'b0001;
-        #50 train_done = 1;  
-        #10 train_done = 0;
-
-// Test 2: Simultaneous request from Train 1 and 4
-        #10 train_request = 4'b1001; 
-        #50 train_done = 1;
-        #10 train_done = 0;
-
-// Test 3: Request by Train 3
-        #10 train_request = 4'b0100;
-        #50 train_done = 1;
-        #10 train_done = 0;
-
-// Test 4: done signal not sent
-        #10 train_request = 4'b0010;
-        #60; 
-
-// Test 5: Multiple Quick Swiching
-        train_done = 1;
-        train_request = 4'b1100;
-        #10 train_done = 0;
-        #20 train_done = 1;
-        train_request = 4'b0000;
-        #10 train_done = 0;
-
-        #50 $finish;
-    end
-
+module TB();
+  wire [2:0] approved_train;
+  reg clk, rst;
+  reg [3:0] train_req;
+  
+  always #5 clk = ~clk;
+  
+  top_module dut(clk, rst, train_req, approved_train);
+  
+  initial begin
+    clk = 1;
+    rst = 1;
+    #15 rst = 0; train_req = 4'b1100;
+    #200 train_req = 4'b0110;
+    $dumpfile ("out.vcd");
+    $dumpvars (0, TB);
+    #200 $finish;
+  end
+  
 endmodule
+  
+  
+    
+  
